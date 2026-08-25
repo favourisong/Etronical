8bd29ce6fca1733353f6b4b557ddf5aed0110527
Benjamin Isong <beisongabc@gmail.com>
2026-08-25 13:05:20 +0100

    Prototype

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+const repoName = "Etronical"
+
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  basePath: process.env.NODE_ENV === "production" ? `/${repoName}` : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? `/${repoName}/` : "",
   typescript: {
     ignoreBuildErrors: true,
   },
